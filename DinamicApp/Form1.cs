03502bcdcfb12cb65bd9c89879b9e31642be04df
Andrew <Andrew@DESKTOP-83F59DH>
2024-03-14 11:05:11 +0300

    Добавьте файлы проекта.

---
+++ DinamicApp/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DinamicApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.MouseClick += MainForm_MouseClick;
+        }
+
+        private void MainForm_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                Button newButton = new Button();
+                newButton.Text = $"({e.X}, {e.Y})";
+                newButton.Location = new System.Drawing.Point(e.X, e.Y);
+                newButton.AutoSize = true;
+                newButton.Click += (s, args) =>
+                {
+                    MessageBox.Show($"Нажата кнопка в позиции: {newButton.Location}");
+                };
+                this.Controls.Add(newButton);
+            }
+        }
+    }
+}
